58590ffef3a282c6e7065029ace6cb52093ffa51
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2025-01-07 15:14:23 +0000

    Increased spacing for readability

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -1,20 +1,26 @@
-# main.py for PowerMonitoring (Basic) assembled from recipe
+"""main.py for PowerMonitoring (Basic) assembled from recipe"""
 
 ## -- Imports ---------------------------------------------------------------------
+
 # standard imports
 from time import sleep
+
 # installed imports
 #none
+
 # local imports from Sensing Service Module version lite-v0.5
 from utilities.mqtt_out import publish
 from hardware.ICs.BCRoboticsADCHAT import BCRoboticsADCHAT
 from hardware.ICs.ads1115 import ADS1115
 from hardware.generic.genericCT import CurrentTransformer
+
 ## --------------------------------------------------------------------------------
 
 
 
+
 ## -- Settings --------------------------------------------------------------------
+
 machine_name = "MyMachine"
 sample_interval = 1 # seconds
 phases = (
@@ -22,11 +28,14 @@ phases = (
     #("L1", BCRoboticsADCHAT(15), CurrentTransformer(gain=20*0.4)),
     ("DC", ADS1115(0), CurrentTransformer(gain=15.65, offset=1.6408)),
 )
+
 ## --------------------------------------------------------------------------------
 
 
 
+
 ## -- PowerMonitoring (Basic) ------------------------------------------------------
+
 while True:
     for phase in phases:
         phase_name = phase[0]
@@ -44,4 +53,5 @@ while True:
             )
 
     sleep(sample_interval)
+
 ## --------------------------------------------------------------------------------
