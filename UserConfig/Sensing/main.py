af4c872dfd383005b0f990bee726241f2c52c112
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2025-01-07 15:39:45 +0000

    Allowed multiple machines per sensing module

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -21,12 +21,15 @@ from hardware.generic.genericCT import CurrentTransformer
 
 ## -- Settings --------------------------------------------------------------------
 
-machine_name = "MyMachine"
 sample_interval = 1 # seconds
+
 phases = (
-# Phase name, ADC and channel, current transformer/clamp/shunt with gain (and offset)
-    #("L1", BCRoboticsADCHAT(15), CurrentTransformer(gain=20*0.4)),
-    ("DC", ADS1115(0), CurrentTransformer(gain=15.65, offset=1.6408)),
+    # Machine name, Phase name, ADC and channel, current transformer/clamp/shunt with gain (and offset)
+    ("Machine_1", "L1", BCRoboticsADCHAT(2), CurrentTransformer(gain=20*0.4)),               # 3pu AC with 20A clamps
+    ("Machine_1", "L2", BCRoboticsADCHAT(4), CurrentTransformer(gain=20*0.4)),
+    ("Machine_1", "L3", BCRoboticsADCHAT(7), CurrentTransformer(gain=20*0.4)),
+    ("Machine_2", "single",      ADS1115(0), CurrentTransformer(gain=50*0.4)),               # 1ph AC with 50A clamp
+    ("Machine_3", "DC",          ADS1115(2), CurrentTransformer(gain=15.65, offset=1.6408)), # single phase DC
 )
 
 ## --------------------------------------------------------------------------------
@@ -38,9 +41,10 @@ phases = (
 
 while True:
     for phase in phases:
-        phase_name = phase[0]
-        adc = phase[1]
-        ct = phase[2]
+        machine_name = phase[0]
+        phase_name = phase[1]
+        adc = phase[2]
+        ct = phase[3]
 
         current = ct(adc.read_voltage())
 
