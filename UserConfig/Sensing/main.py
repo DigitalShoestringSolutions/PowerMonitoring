c77218f67f2f7220244e56dfbee4ca5aff522d9c
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2025-01-22 16:36:35 +0000

    Added example machine using Sequent ADC

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -12,6 +12,7 @@ from time import sleep
 from utilities.mqtt_out import publish
 from hardware.ICs.BCRoboticsADCHAT import BCRoboticsADCHAT
 from hardware.ICs.ads1115 import ADS1115
+from hardware.ICs.sequent_16_universal_inputs import Sequent_16chADC
 from hardware.generic.genericCT import CurrentTransformer
 
 ## --------------------------------------------------------------------------------
@@ -27,9 +28,11 @@ phases = (
     # Machine name, Phase name, ADC and channel, current transformer/clamp/shunt with gain (and offset)
     ("Machine_1", "L1", BCRoboticsADCHAT(2), CurrentTransformer(gain=20*0.4)),               # 3pu AC with 20A clamps
     ("Machine_1", "L2", BCRoboticsADCHAT(4), CurrentTransformer(gain=20*0.4)),
-    ("Machine_1", "L3", BCRoboticsADCHAT(7), CurrentTransformer(gain=20*0.4)),
+    ("Machine_1", "L3", BCRoboticsADCHAT(7), CurrentTransformer(20*0.4)),                    # gain is first positional arg, does not need to be explicit
     ("Machine_2", "single",      ADS1115(0), CurrentTransformer(gain=50*0.4)),               # 1ph AC with 50A clamp
     ("Machine_3", "DC",          ADS1115(2), CurrentTransformer(gain=15.65, offset=1.6408)), # single phase DC
+    ("Machine_4", "L1",  Sequent_16chADC(1), CurrentTransformer(30*0.4)),                    # 2pu with 30A clamp on L1
+    ("Machine_4", "L2",  Sequent_16chADC(3), CurrentTransformer(10*0.4)),                    #     but only 10A on L2
 )
 
 ## --------------------------------------------------------------------------------
