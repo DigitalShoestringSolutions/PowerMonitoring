5cbda18e0e91ed2142ee63cb50bed65fec624ebc
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2025-10-09 13:12:03 +0100

    Switched to external analysis v0.2.0

--- custom_service_modules/analysis/code/format/general.py
+++
@@ -1,7 +0,0 @@
-
-def timestamp_to_iso_format():
-    async def action(data):
-        for entry in data:
-            entry["timestamp"] = entry["timestamp"].isoformat()
-        return data
-    return action
